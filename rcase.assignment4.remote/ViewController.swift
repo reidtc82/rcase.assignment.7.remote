b40d7264e64847367c150c810a75f18e9004edfc
reidtc82 <reidtc82@gmail.com>
2018-02-07 15:03:45 -0800

    Initial Commit

---
+++ rcase.assignment4.remote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rcase.assignment4.remote
+//
+//  Created by Reid Case on 2/7/18.
+//  Copyright © 2018 DePaul University CSC471. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ rcase.assignment4.remoteTests/rcase_assignment4_remoteTests.swift
@@ -0,0 +1,36 @@
+//
+//  rcase_assignment4_remoteTests.swift
+//  rcase.assignment4.remoteTests
+//
+//  Created by Reid Case on 2/7/18.
+//  Copyright © 2018 DePaul University CSC471. All rights reserved.
+//
+
+import XCTest
+@testable import rcase_assignment4_remote
+
+class rcase_assignment4_remoteTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
